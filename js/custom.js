34b890172fc07be5f461c56967bed71daa25ae4e
Oscar <owaizel@gmail.com>
2020-12-03 20:32:28 -0800

    Shit on stuff i dont even know

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+(function ($) {
+
+
+    // HEADER
+    $(".navbar").headroom();
+
+    // PROJECT CAROUSEL
+    $('.owl-carousel').owlCarousel({
+    	items: 1,
+	    loop:true,
+	    margin:10,
+	    nav:true
+	});
+
+
+})(jQuery);
